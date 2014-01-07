2bc21a125a5a6fe953e94b23f91a9ea0281a56bb
Satish Goda <satishg@rhythm.com>
2014-01-06 21:49:46 -0800

    Localized scope of variables to the loop

--- cpp/iterators/printreverse.cpp
+++ cpp/iterators/printreverse.cpp
@@ -9,17 +9,16 @@ int main(int argc, char* argv[])
    
     vector<string> cmdlineargs(argv+1, argv+argc);
    
-    auto rbiterv = cmdlineargs.rbegin();
-   
-    while( rbiterv != cmdlineargs.rend() ) {
-        auto& arg = *rbiterv++;
-        auto rbiterarg = arg.rbegin();
-       
-        while( rbiterarg != arg.rend() ) {
-            cout << *rbiterarg++;
+    for(auto rbiterv=cmdlineargs.rbegin(); rbiterv != cmdlineargs.rend(); ++rbiterv) {
+        auto& arg = *rbiterv;
+        
+        for(auto rbiterarg = arg.rbegin(); rbiterarg != arg.rend(); ++rbiterarg) {
+            cout << *rbiterarg;    
         }
+        
         cout << endl;
     }
    
     return EXIT_SUCCESS;
-} 
+}
+
